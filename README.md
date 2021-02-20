069b7ea11a22c3db085d192e167fbdf365bdafd8
SymfonyCordova <978451251@qq.com>
2021-02-20 14:44:51 +0800

    add laravel-provider and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # zler-wechat-sdk
+
+An API extension for WeChat client sdk
+
+# install
+
+- sample install
+
+  ```shell
+  composer require zler/wechat
+  ```
+
+- laravel install
+
+  ```shell
+  php artisan vendor:publish --provider="Zler\Wechat\Provider\LaravelServiceProvider"
+  ```
+
+  
--- src/Provider/LaravelServiceProvider.php
+++ src/Provider/LaravelServiceProvider.php
@@ -32,7 +32,7 @@ class LaravelServiceProvider extends ServiceProvider implements DeferrableProvid
     public function boot()
     {
         $this->publishes([
-            __DIR__.'config/zler.php' => config_path('zler.php'),
+            __DIR__.'/config/zler.php' => config_path('zler.php'),
         ]);
     }
 
