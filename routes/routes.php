f5f7f8b0acc9e552dd997970695b10e033b2558d
SymfonyCordova <978451251@qq.com>
2021-02-20 16:11:46 +0800

    update laravel provider router middle

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => config('zler.router.prefix')], function (){
+Route::group(['prefix' => config('zler.router.prefix'),'middleware' => [''] ], function (){
     Route::get('/token', '\Zler\Wechat\Controller\GzhController@token');
 });
