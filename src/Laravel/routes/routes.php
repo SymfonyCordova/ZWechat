35fae682c148c321e48a3c61ab642f18d08db296
SymfonyCordova <978451251@qq.com>
2021-02-22 08:57:32 +0800

    routes

--- src/Laravel/routes/routes.php
+++ src/Laravel/routes/routes.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => config('zler.router.prefix'),'middleware' => [] ], function (){
+Route::group(['prefix' => config('zler-wechat.router.prefix'),'middleware' => [] ], function (){
     Route::get('/token', '\Zler\Wechat\Laravel\Controller\GzhController@token');
 });
