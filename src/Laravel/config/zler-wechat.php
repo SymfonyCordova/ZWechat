11bf26c1a98aac5e96b5c75f502d32f16c92988f
SymfonyCordova <978451251@qq.com>
2021-02-25 16:31:36 +0800

    gzh accesstoken path

--- src/Laravel/config/zler-wechat.php
+++ src/Laravel/config/zler-wechat.php
@@ -7,7 +7,7 @@ return [
         'app_id' => env('gzh_app_id', ''),
         'app_secret' => env('gzh_app_secret', ''),
         'token' => env('gzh_token', ''),
-        'access_token_path' => '',
-        'js_ticket_path' => ''
+        'access_token_path' => public_path('vendor/zler-wechat').'/access_token.txt',
+        'js_ticket_path' => public_path('vendor/zler-wechat').'/js_ticket.txt',
     ]
 ];
